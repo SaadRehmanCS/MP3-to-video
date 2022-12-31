dddbfbb3c4e0eda38d0e0ba216a46308057b30b2
Saad Rehman <saad@dhcp-128-189-85-4.ubcsecure.wireless.ubc.ca>
2023-01-01 00:36:09 +0400

    auth service is complete

---
+++ src/auth/init.sql
@@ -0,0 +1,14 @@
+create USER 'auth_user'@'localhost' IDENTIFIED BY 'Aauth123';
+create DATABASE auth;
+
+grant all privileges on auth.* to 'auth_user'@'localhost';
+
+USE auth;
+
+create table user (
+    id INT not null AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) not null unique,
+    password VARCHAR(255) not null
+);
+
+insert INTO user (email, password) values ('saadrehman2000@gmail.com', 'rehman123')
